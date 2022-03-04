e40a711147972a6e4711b12808db49689bfeebca
SayanthD <13906889+SayanthD@users.noreply.github.com>
2022-03-04 05:50:53 +0530

    signin: Create basic layout

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '/src/signup.html'));
 });
 
+app.get('/signin', (req, res) => {
+	res.sendFile(path.join(__dirname + '/src/signin.html'));
+});
+
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
 })
